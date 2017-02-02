eece618130336c737b20175ce3e2b1f8055bc1ce
matthew-french <matthew.french@gmail.com>
2017-02-02 13:41:14 +0000

    First commit: Basic webpack babel template

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Basic Webpack and Babel'
+    })
+  ]
+};
