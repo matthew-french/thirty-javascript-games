e64d7b21aa82af2f66f07dbeaccd4dec9faa5d88
matthew-french <matthew.french@gmail.com>
2017-03-01 18:24:43 +0000

    React refactor

---
+++ src/components/GamePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GamePicker = () => {
+    return (
+      <ul>
+        <li><a href="Drumkit">Drumkit</a></li>
+      </ul>
+    )
+}
+
+export default GamePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-document.write('hello world');
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss} from 'react-router';
+
+import GamePicker from './components/GamePicker';
+import Drumkit from './components/Drumkit';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={GamePicker} />
+        <Match pattern="/drumkit" component={Drumkit} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
